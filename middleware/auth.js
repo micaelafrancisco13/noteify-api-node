1c62b25928f831f69618a3cb3cb0c54b9f56d10e
Micaela Francisco <micaela.francisco.s@bulsu.edu.ph>
2023-08-21 22:15:51 +0800

    change private key from custom jwt private key to keycloak public key

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,11 +12,20 @@ module.exports = function (req, res, next) {
 
   try {
     // this returns the payload defined on the generateAuthToken()
-    const decodedPayload = jwt.verify(token, config.get("JWT_PRIVATE_KEY"));
+    const privateKey = `-----BEGIN PUBLIC KEY-----\n${config.get("JWT_PRIVATE_KEY")}\n-----END PUBLIC KEY-----`;
+    const options = {
+      algorithms: ["RS256"],
+    };
+    const decodedPayload = jwt.verify(
+      token,
+      privateKey,
+      options
+    );
+    console.log("decodedPayload", decodedPayload);
     req.user = decodedPayload;
 
     next();
   } catch (ex) {
-    res.status(400).send("Invalid token.");
+    res.status(400).send(`${ex.message.toUpperCase()}.`);
   }
 };
